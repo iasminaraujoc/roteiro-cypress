ec5a88cfe22a1084acf3b1e2e7efc3b5e1a5df25
iasminaraujoc <iasmincorreaaraujo@gmail.com>
2024-07-03 18:32:06 -0300

    testes de integração

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -56,4 +56,78 @@ describe('template spec', () => {
     cy.get('.todo-list li')
       .should('have.length', 2);
   });
+
+  it('Completa tarefas cadastradas', () => {
+    cy.visit('http://127.0.0.1:7001');
+    
+    cy.get('.new-todo')
+     .type('TP2 de ES{enter}');
+
+    cy.get('.todo-list li .toggle')
+     .first()
+     .click();
+
+    cy.get('.toggle-all')
+     .click(); 
+
+    cy.contains('Clear completed').click();
+    cy.get('.todo-list li')
+      .should('have.length', 0);
+  
+});
+
+  it('Marcar todas as tarefas como ativas', ()=>{
+    cy.visit('http://127.0.0.1:7001');
+    
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+      cy.get('.toggle-all')
+      .click();
+
+      cy.get('.toggle-all')
+      .click();
+
+      cy.contains('Active').click();
+      cy.get('.todo-list li')
+        .should('have.length', 2)
+        .first()
+        .should('have.text', 'TP2 de ES');
+  }
+  );
+
+  it('Editar tarefas', ()=>{
+    cy.visit('http://127.0.0.1:7001');
+    
+    cy.get('.new-todo')
+     .type('TP2 de ES{enter}')
+     .type('Prova de ES{enter}');
+
+    cy.get('.todo-list li')
+     .first()
+     .find('label')
+     .dblclick();
+    
+     cy.get('.edit')
+     .clear()
+     .type('TP2 de Engenharia de Software{enter}');
+    
+     cy.get('.todo-list li')
+     .first()
+     .should('have.text', 'TP2 de Engenharia de Software');
+
+     cy.get('.todo-list li')
+     .first()
+     .find('label')
+     .dblclick();
+    
+     cy.get('.edit')
+     .clear()
+     .type('Prova de Engenharia de Software{enter}');
+    
+     cy.get('.todo-list li')
+     .first()
+     .should('have.text', 'Prova de Engenharia de Software');
+  });
 });
